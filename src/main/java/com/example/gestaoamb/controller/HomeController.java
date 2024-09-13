eaa0b7bdfb0fcf2fb095322f728e582f24e6da9b
heloghirardeloo <hghirardello008@gmail.com>
2024-09-13 10:01:06 -0300

    Criando mecanismos de segurança como o login e cadastro

--- src/main/java/com/example/gestaoamb/controller/HomeController.java
+++ src/main/java/com/example/gestaoamb/controller/HomeController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+@RequestMapping("/home")
+public class HomeController {
 
     @GetMapping
-    public String formLogin (){
-        return "login/login";
+    public String home(){
+        return "home/index";
     }
+
 }
